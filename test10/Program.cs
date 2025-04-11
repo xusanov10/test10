bcc6b2ff0be70dfda37d9318dc64b7a2f9d32822
xusanov10 <ahmadjonxusanovv>
2025-04-11 15:06:01 +0500

    cahnge15

--- test10/Program.cs
+++ test10/Program.cs
@@ -13,3 +13,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
